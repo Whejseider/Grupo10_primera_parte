8a2c2e593e9b09b2e8103a560763b5317b735bf9
blautista <bautistaromanazzi@gmail.com>
2023-04-25 18:50:21 -0300

    add: decorator de pago de abonado

---
+++ src/negocio/PagoDecorator.java
@@ -0,0 +1,31 @@
+package negocio;
+
+import negocio.interfaces.IFactura;
+import negocio.interfaces.IPromocion;
+
+public abstract class PagoDecorator implements IFactura {
+	private IFactura facturable;
+	
+	/**
+	 * Crea un nuevo decorator a partir de una clase que implemente IFacturable.
+	 * @param facturable El facturable a decorar
+	 */
+	public PagoDecorator(IFactura facturable) {
+		assert facturable != null : "El facturable no puede ser nulo";
+		this.facturable = facturable;
+	}
+	
+	protected IFactura getFacturable() {
+		return this.facturable;
+	}
+	
+	@Override 
+	public double getPagoNeto(IPromocion promo) {
+		return this.getFacturable().getPagoNeto(promo);
+	}
+
+	@Override
+	public String getDetalle() {
+		return this.getFacturable().getDetalle();
+	}
+}
---
+++ src/negocio/PagoEfectivoDecorator.java
@@ -0,0 +1,17 @@
+package negocio;
+
+import negocio.interfaces.IFactura;
+import negocio.interfaces.IPromocion;
+
+public class PagoEfectivoDecorator extends PagoDecorator {
+	private final double modificador = 0.8;
+	
+	public PagoEfectivoDecorator(IFactura facturable) {
+		super(facturable);
+	}
+
+	@Override
+	public double getPagoMedioDePago(IPromocion promo) {
+		return this.getFacturable().getPagoNeto(promo) * modificador;
+	}
+}
