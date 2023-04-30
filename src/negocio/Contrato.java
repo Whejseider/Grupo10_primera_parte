88fba7ea084b31ca0a746a95bc412f679a05665d
blautista <bautistaromanazzi@gmail.com>
2023-04-30 00:17:29 -0300

    refactor: formato de codigo en Contrato

--- src/negocio/Contrato.java
+++ src/negocio/Contrato.java
@@ -5,140 +5,140 @@ import negocio.interfaces.IPromocion;
 import negocio.interfaces.IServicioAlarma;
 
 public abstract class Contrato implements IContrato {
-	private static int numero = 0;
-	private int ID;
-	private String domicilio;
-	private IServicioAlarma servicioAlarma;
+    private static int numero = 0;
+    private int ID;
+    private String domicilio;
+    private IServicioAlarma servicioAlarma;
 
-	/**
-	 * Constructor de la clase Contrato.
-	 * 
-	 * <b>pre: </b>: El domilicio debe estar definido y no ser vacío. El objeto
-	 * servicio no puede ser nulo.
-	 * 
-	 * @param domicilio      El domicilio donde quedará registrado el contrato. Este
-	 *                       se usa para verificar igualdad entre contratos.
-	 * @param servicioAlarma Los servicios adicionales a adjuntar al contrato.
-	 */
-	public Contrato(String domicilio, IServicioAlarma servicioAlarma) {
-		assert (domicilio != null);
-		assert (servicioAlarma != null);
-		this.ID = numero++;
-		this.servicioAlarma = servicioAlarma;
-		this.domicilio = domicilio;
-	}
+    /**
+     * Constructor de la clase Contrato.
+     * 
+     * <b>pre: </b>: El domilicio debe estar definido y no ser vacío. El objeto
+     * servicio no puede ser nulo.
+     * 
+     * @param domicilio      El domicilio donde quedará registrado el contrato. Este
+     *                       se usa para verificar igualdad entre contratos.
+     * @param servicioAlarma Los servicios adicionales a adjuntar al contrato.
+     */
+    public Contrato(String domicilio, IServicioAlarma servicioAlarma) {
+        assert (domicilio != null);
+        assert (servicioAlarma != null);
+        this.ID = numero++;
+        this.servicioAlarma = servicioAlarma;
+        this.domicilio = domicilio;
+    }
 
-	/**
-	 * Devuelve el domicilio vinculado al contrato
-	 * 
-	 * @return El domicilio del contrato
-	 */
-	public String getDomicilio() {
-		return this.domicilio;
-	}
+    /**
+     * Devuelve el domicilio vinculado al contrato
+     * 
+     * @return El domicilio del contrato
+     */
+    public String getDomicilio() {
+        return this.domicilio;
+    }
 
-	/**
-	 * Agrega camaras al contrato
-	 * 
-	 * <b>pre:</b> La cantidad no puede ser menor a cero.
-	 * 
-	 * @param cantidad La cantidad de camaras a agregar
-	 */
-	public void agregarCamaras(int cantidad) {
-		assert (cantidad >= 0);
-		this.servicioAlarma.agregarCamaras(cantidad);
-	}
+    /**
+     * Agrega camaras al contrato
+     * 
+     * <b>pre:</b> La cantidad no puede ser menor a cero.
+     * 
+     * @param cantidad La cantidad de camaras a agregar
+     */
+    public void agregarCamaras(int cantidad) {
+        assert (cantidad >= 0);
+        this.servicioAlarma.agregarCamaras(cantidad);
+    }
 
-	/**
-	 * Agrega botones antipánico al contrato.
-	 * 
-	 * <b>pre:</b> La cantidad no puede ser menor a cero.
-	 * 
-	 * @param cantidad La cantidad de botones a agregar
-	 */
-	public void agregarBtnesAntipanico(int cantidad) {
-		assert (cantidad >= 0);
-		this.servicioAlarma.agregarBotonesAntipanico(cantidad);
-	}
+    /**
+     * Agrega botones antipánico al contrato.
+     * 
+     * <b>pre:</b> La cantidad no puede ser menor a cero.
+     * 
+     * @param cantidad La cantidad de botones a agregar
+     */
+    public void agregarBtnesAntipanico(int cantidad) {
+        assert (cantidad >= 0);
+        this.servicioAlarma.agregarBotonesAntipanico(cantidad);
+    }
 
-	/**
-	 * Agrega camaras al contrato
-	 * 
-	 * <b>pre:</b> La cantidad no puede ser menor a cero.
-	 * 
-	 * @param cantidad
-	 */
-	public void contratarMovilAcompaniamiento() {
-		this.servicioAlarma.contratarMovil();
-	}
+    /**
+     * Agrega camaras al contrato
+     * 
+     * <b>pre:</b> La cantidad no puede ser menor a cero.
+     * 
+     * @param cantidad
+     */
+    public void contratarMovilAcompaniamiento() {
+        this.servicioAlarma.contratarMovil();
+    }
 
-	/**
-	 * Obtiene el precio base del contrato. Varía dependiendo del tipo de contrato.
-	 */
-	protected abstract double getPrecioBase();
+    /**
+     * Obtiene el precio base del contrato. Varía dependiendo del tipo de contrato.
+     */
+    protected abstract double getPrecioBase();
 
-	/**
-	 * Obtiene el precio total del contrato, sin promociones aplicadas.
-	 */
-	public double getPrecio() {
-		return this.servicioAlarma.getPrecio() + getPrecioBase();
-	}
+    /**
+     * Obtiene el precio total del contrato, sin promociones aplicadas.
+     */
+    public double getPrecio() {
+        return this.servicioAlarma.getPrecio() + getPrecioBase();
+    }
 
-	/**
-	 * Genera una string con el detalle del contrato, con su total y contrataciones.
-	 */
-	public String getDetalle(IPromocion promo) {
-		StringBuilder detalle = new StringBuilder();
+    /**
+     * Genera una string con el detalle del contrato, con su total y contrataciones.
+     */
+    public String getDetalle(IPromocion promo) {
+        StringBuilder detalle = new StringBuilder();
 
-		detalle.append("Contrato N°" + this.ID + "\n");
-		detalle.append(this.servicioAlarma.getDetalle());
-		detalle.append("Costo $" + this.getPrecio(promo) + "\n");
+        detalle.append("Contrato N°" + this.ID + "\n");
+        detalle.append(this.servicioAlarma.getDetalle());
+        detalle.append("Costo $" + this.getPrecio(promo) + "\n");
 
-		return detalle.toString();
-	}
+        return detalle.toString();
+    }
 
-	/**
-	 * Devuelve verdadero si dos contratos se consideran iguales. Son iguales si
-	 * coincide su domicilio.<br>
-	 * 
-	 * @param obj El objeto a comparar.
-	 * @return Verdadero si son iguales.
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == this) {
-			return true;
-		}
+    /**
+     * Devuelve verdadero si dos contratos se consideran iguales. Son iguales si
+     * coincide su domicilio.<br>
+     * 
+     * @param obj El objeto a comparar.
+     * @return Verdadero si son iguales.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
 
-		if (!(obj instanceof Contrato)) {
-			return false;
-		}
+        if (!(obj instanceof Contrato)) {
+            return false;
+        }
 
-		Contrato contrato = (Contrato) obj;
+        Contrato contrato = (Contrato) obj;
 
-		// No es necesario fijarse si el ID es igual porque es autoincremental.
-		return this.domicilio.equals(contrato.getDomicilio());
-	}
+        // No es necesario fijarse si el ID es igual porque es autoincremental.
+        return this.domicilio.equals(contrato.getDomicilio());
+    }
 
-	/**
-	 * Clona el contrato
-	 */
-	@Override
-	public IContrato clone() throws CloneNotSupportedException {
-		Contrato contratoCloneado = (Contrato) super.clone();
+    /**
+     * Clona el contrato
+     */
+    @Override
+    public IContrato clone() throws CloneNotSupportedException {
+        Contrato contratoCloneado = (Contrato) super.clone();
 
-		contratoCloneado.servicioAlarma = (ServicioAlarma) this.servicioAlarma.clone();
+        contratoCloneado.servicioAlarma = (ServicioAlarma) this.servicioAlarma.clone();
 
-		return contratoCloneado;
-	}
+        return contratoCloneado;
+    }
 
-	/**
-	 * Obtiene el precio con descuento por promoción del contrato.
-	 * 
-	 * <b>pre:</b> La promocion debe estar definida
-	 * 
-	 * @param promocionActual La promo a aplicar.
-	 */
-	public abstract double getPrecio(IPromocion promocionActual);
+    /**
+     * Obtiene el precio con descuento por promoción del contrato.
+     * 
+     * <b>pre:</b> La promocion debe estar definida
+     * 
+     * @param promocionActual La promo a aplicar.
+     */
+    public abstract double getPrecio(IPromocion promocionActual);
 
 }
