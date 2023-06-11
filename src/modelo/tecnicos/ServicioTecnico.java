9d4fb05bef1b3e8baec0982e6ae0c8ca1a05ceac
blautista <bautistaromanazzi@gmail.com>
2023-06-11 18:48:34 -0300

    add: comentarios en Tecnico y ServicioTecnico

--- src/modelo/tecnicos/ServicioTecnico.java
+++ src/modelo/tecnicos/ServicioTecnico.java
@@ -4,6 +4,10 @@ import modelo.interfaces.IAbonado;
 
 import java.util.Observable;
 
+/**
+ * Clase que representa un servicio tecnico realizado o a realizar
+ * de un abonado.
+ */
 public class ServicioTecnico extends Observable implements Runnable {
     private final IAbonado abonado;
     private final Tecnico tecnico;
@@ -11,17 +15,29 @@ public class ServicioTecnico extends Observable implements Runnable {
     private final int intervaloProgreso = 10;
     private Estado estado = Estado.ESPERANDO_TECNICO;
 
+    /**
+     * El estado actual del servicio
+     */
     public enum Estado {
         ESPERANDO_TECNICO,
         EN_CURSO,
         FINALIZADO
     }
 
+    /**
+     * Constructor de la clase ServicioTecnico.
+     * @param abonado El abonado a asignar.
+     * @param tecnico El tecnico a asignar.
+     */
     public ServicioTecnico(IAbonado abonado, Tecnico tecnico) {
         this.abonado = abonado;
         this.tecnico = tecnico;
     }
 
+    /**
+     * Algoritmo que simula el avance del service. Notifica los progresos
+     * a sus observers.
+     */
     private void avanzar() {
 
         while (progreso < 100) {
@@ -42,22 +58,39 @@ public class ServicioTecnico extends Observable implements Runnable {
         notifyObservers(100);
     }
 
+    /**
+     * @return Verdadero si el service ha finalizado.
+     */
     public boolean getTerminado() {
         return this.progreso == 100;
     }
 
+    /**
+     * Obtiene el progreso actual del service.
+     * @return Un numero entre 0 y 100 que indica el avance del service.
+     */
     public int getProgreso() {
         return progreso;
     }
 
+    /**
+     * Obtiene el abonado asignado al service
+     */
     public IAbonado getAbonado() {
         return abonado;
     }
 
+    /**
+     * Obtiene el tecnico asignado al service
+     */
     public Tecnico getTecnico() {
         return tecnico;
     }
 
+    /**
+     * Obtiene texto con información del estado actual del servicio.
+     * @return Una string para mostrar en pantalla.
+     */
     public String getTextoEstado() {
         switch (this.estado) {
             case EN_CURSO:
@@ -71,10 +104,20 @@ public class ServicioTecnico extends Observable implements Runnable {
         }
     }
 
+    /**
+     * Devuelve el estado actual del servicio
+     */
     public Estado getEstado() {
         return estado;
     }
 
+    /**
+     * Inicia el servicio. Intenta asignar el abonado al tecnico.
+     *
+     * - Si el técnico está disponible, se pone a trabajar inmediatamente.
+     * - Si el técnico no está disponible, se espera a que finalice su servicio actual y pendientes
+     * para asignarse.
+     */
     @Override
     public void run() {
         this.estado = Estado.ESPERANDO_TECNICO;
--- src/modelo/tecnicos/Tecnico.java
+++ src/modelo/tecnicos/Tecnico.java
@@ -4,33 +4,61 @@ import java.io.Serializable;
 
 import modelo.interfaces.IAbonado;
 
+/**
+ * Clase que representa a un tecnico del sistema
+ */
 public class Tecnico implements Serializable {
     private static int numero = 0;
     private final String nombre;
     private boolean disponible = true;
     private final int id;
 
+    /**
+     * Constructor de la clase tecnico
+     * @param nombre El nombre a usar
+     */
     public Tecnico(String nombre) {
         this.nombre = nombre;
         this.id = numero++;
     }
 
+    /**
+     * Obtiene el id del tecnico
+     * @return El id del tecnico
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Obtiene el nombre del técnico
+     * @return El nombre del técnico
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Obtiene la disponibilidad del tecnico.
+     * @return Verdadero si no está realizando ningún service
+     */
     public boolean isDisponible() {
         return disponible;
     }
 
+    /**
+     * Sobreescribe el estado disponible del tecnico. SOLO PARA SERIALIZACION.
+     * @param disponible El nuevo estado.
+     */
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
 
+    /**
+     * Asigna un tecnico. Mientras esté asignado, no se lo puede llamar a otros service. Si
+     * el tecnico ya estaba asignado, espera hasta que sea liberado para asignarse.
+     * @param abonado El abonado a asignar.
+     */
     public synchronized void asignarAbonado(IAbonado abonado) throws InterruptedException {
         while (!disponible) {
             wait();
@@ -42,6 +70,9 @@ public class Tecnico implements Serializable {
         notifyAll();
     }
 
+    /**
+     * Libera el tecnico de sus funciones, y vuelve a estar disponible.
+     */
     public synchronized void liberar() {
         this.disponible = true;
         notifyAll();
